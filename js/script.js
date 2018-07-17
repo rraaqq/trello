068ab8b2ab363c08b84be3ac22ddc7ee66877087
Michal Raczkowski <michal.raczkowski.88@gmail.com>
2018-07-17 20:49:38 +0100

    Add app logic

---
+++ js/script.js
@@ -0,0 +1,111 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Board Object
+
+    var board = {
+        name: 'Kanban Board',
+        addColumn: function (column) {
+            this.element.appendChild(column.element);
+            initSortable(column.id);
+        },
+        element: document.querySelector('#board .column-container')
+    };
+
+    // Class Column
+
+    function Column(name) {
+        var self = this;
+
+        this.id = randomString();
+        this.name = name;
+        this.element = generateTemplate('column-template', {
+            name: this.name,
+            id: this.id
+        });
+
+        this.element.querySelector('.column').addEventListener('click', function (event) {
+            if (event.target.classList.contains('btn-delete')) {
+                self.removeColumn();
+            }
+
+            if (event.target.classList.contains('add-card')) {
+                self.addCard(new Card(prompt("Enter the name of the card")));
+            }
+        });
+    };
+    Column.prototype = {
+        addCard: function (card) {
+            this.element.querySelector('ul').appendChild(card.element);
+        },
+        removeColumn: function () {
+            this.element.parentNode.removeChild(this.element);
+        }
+    };
+
+    // Class Card
+
+    function Card(description) {
+        var self = this;
+
+        this.id = randomString();
+        this.description = description;
+        this.element = generateTemplate('card-template', {
+            description: this.description
+        }, 'li');
+
+        this.element.querySelector('.card').addEventListener('click', function (event) {
+            event.stopPropagation();
+
+            if (event.target.classList.contains('btn-delete')) {
+                self.removeCard();
+            }
+        });
+    };
+    Card.prototype = {
+        removeCard: function () {
+            this.element.parentNode.removeChild(this.element);
+        }
+    };
+
+    // Template
+
+    function generateTemplate(name, data, basicElement) {
+        var template = document.getElementById(name).innerHTML;
+        var element = document.createElement(basicElement || 'div');
+
+        Mustache.parse(template);
+        element.innerHTML = Mustache.render(template, data);
+
+        return element;
+    }
+
+    // Function Sortable
+
+    function initSortable(id) {
+        var el = document.getElementById(id);
+        var sortable = Sortable.create(el, {
+            group: 'kanban',
+            sort: true
+        });
+    }
+
+    // Function Generate ID
+
+    function randomString() {
+        var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+        var str = '';
+        for (var i = 0; i < 10; i++) {
+            str += chars[Math.floor(Math.random() * chars.length)];
+        }
+        return str;
+    }
+
+    // New column button
+
+    document.querySelector('#board .create-column').addEventListener('click', function () {
+        var name = prompt('Enter a column name');
+        var column = new Column(name);
+        board.addColumn(column);
+    });
+
+});
